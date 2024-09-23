88b51d1580a9a0d74ff6f636552c0907843db563
beortizz <beortizz@gmail.com>
2024-09-23 13:53:49 -0300

    add phone number to user model adn update profile form

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -46,6 +46,11 @@
                 </div>
             @endif
         </div>
+        <div>
+            <x-input-label for="phone_number" :value="__('Phone Number')" />
+            <input type="number" id="phone_number" name="phone_number" type="text" class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" value="{{old('phone_number', $user->phone_number)}}" required autofocus autocomplete="phone_number" />
+            <x-input-error class="mt-2" :messages="$errors->get('phone_number')" />
+        </div>
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
